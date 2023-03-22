0a3bcb3e56fc84b7386771c60d81ade7b5bde433
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2023-03-22 16:33:22 -0700

    Created PGY Levels migration

---
+++ database/migrations/2023_03_22_232922_create_pgy_levels_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+    //1. Create the pgy_levels table.
+    Schema::create('pgy_levels', function (Blueprint $table) {
+        $table->id();
+        $table->string('level');
+        $table->timestamps();
+    });
+
+    //2. Insert levels data into the database as part of the migration.
+    $levels = [
+        'PGY 1',
+        'PGY 2',
+        'PGY 3',
+        'PGY 4',
+        'PGY 5',
+        'PGY 6',
+        'PGY 7',
+        'Research'
+    ];
+
+    foreach ($levels as $level) {
+        DB::table('pgy_levels')->insert([
+            'level' => $level,
+        ]);
+}
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pgy_levels');
+    }
+};
