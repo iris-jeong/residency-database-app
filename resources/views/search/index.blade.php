ca10ecd11f451e5edc7f9afc13db5ec90ac36b9c
Victor Muljo <muljo@usc.edu>
2023-03-23 13:21:38 -0700

    search: added links to run report pages for testing convenience

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -5,4 +5,17 @@
 @endpush
 @section('content')
 <h1>Search</h1>
+
+<ul id="run-report-routes" class="d-flex flex-row justify-content-between">
+    <li id="test-li">(WIP, for testing convenience: )</li>
+    <li id="test-report" class="nav-item">
+        <a href="{{route('search.test')}}" class="nav-link">Test Report</a>
+    </li>
+    <li id="demo-report" class="nav-item">
+        <a href="{{route('search.demographic')}}" class="nav-link">Demographic Report</a>
+    </li>
+    <li id="license-report" class="nav-item">
+        <a href="{{route('search.license')}}" class="nav-link">License Report</a>
+    </li>
+</ul>
 @endsection
