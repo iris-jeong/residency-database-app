0d383994b75e4ef93ba04cb7f3ebb1638a42a344
jeffrey-k-lee <122771401+jeffrey-k-lee@users.noreply.github.com>
2023-03-28 18:59:56 -0700

    Completed search bar

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -18,4 +18,47 @@
         <a href="{{route('search.license')}}" class="nav-link">License Report</a>
     </li>
 </ul>
+
+<div class="input-group mb-3" id="search-bar">
+    <span class="input-group-text" id="search-bar-head"><img src="{{asset('icons/search.svg')}}" class="icons" id="search-bar-icon-head"/></span>
+    <input type="text" id="searchinput" placeholder="Enter name" class="form-control shadow-none">
+    <select class="form-select shadow-none" id="specialty-dropdown">
+        <option value="0">Specialty</option>
+        <option value="1">Abdominal Transplant</option>
+        <option value="2">Advanced GI MIS</option>
+        <option value="3">Breast Oncology</option>
+        <option value="4">Cardiothoracic</option>
+        <option value="5">Research</option>
+        <option value="6">Colorectal</option>
+        <option value="7">General</option>
+        <option value="8">Hepatobiliary</option>
+        <option value="9">Plastics & Reconstructive</option>
+        <option value="10">Surgical & Critical Care</option>
+        <option value="11">Trauma</option>
+        <option value="12">Vascular</option>
+    </select>
+    <select class="form-select shadow-none" id="pgy-dropdown">
+        <option value="0">PGY Level</option>
+        <option value="1">PGY 1</option>
+        <option value="2">PGY 2</option>
+        <option value="3">PGY 3</option>
+        <option value="4">PGY 4</option>
+        <option value="5">Research</option>
+        <option value="6">PGY 4</option>
+        <option value="7">PGY 5</option>
+        <option value="8">PGY 6</option>
+        <option value="8">PGY 7</option>
+    </select>
+    <select class="form-select shadow-none" id="license-dropdown">
+        <option value="0">License</option>
+        <option value="1">ACLS</option>
+        <option value="2">ATLS</option>
+        <option value="3">BLS</option>
+        <option value="4">CML</option>
+        <option value="5">DEA</option>
+        <option value="6">FCCS</option>
+    </select>
+    <button class="btn btn-light" id="search-btn" type="button"><img src="{{asset('icons/search.svg')}}" class="icons" id="search-bar-icon"/>Search</button>
+</div>
+
 @endsection
