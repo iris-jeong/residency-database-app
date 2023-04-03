add6e3976745aefdcc533f1269822e94314a45be
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2023-04-02 18:46:33 -0700

    Connected the settings page to the backend with the database on Heroku

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -8,7 +8,13 @@ use App\Models\User;
 class SettingsController extends Controller
 {
     public function index() {
+        //Retrieve all users that are admins
+        $admins = User::where('access_level_id', 2)->get();
+        $numAdmins = count($admins);
 
-        return view('settings.index');
+        return view('settings.index', [
+            'admins' => $admins, 
+            'numAdmins' => $numAdmins
+        ]);
     }
 }
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -27,9 +27,27 @@
 		</div>
 		<hr>
 
-	<p> Admin Users <span id="admin-count">(7 users)</span></p>
+	<p> Admin Users <span id="admin-count">({{$numAdmins}} users)</span></p>
+	{{-- <p> Admin Users <span id="admin-count">(7 users)</span></p> --}}
 
 	<ul id="admin-users">
+
+		@foreach( $admins as $admin ) 
+			<li class="d-flex justify-content-between align-items-center">
+				<div id="admin-wh" class="d-flex align-items-center">
+					<div class="admin-pic"></div>
+					<div class="admin-info">
+						<p class="admin-name">{{$admin->first_name}} {{$admin->last_name}}</p>
+						<p class="admin-email">{{$admin->email}}</p>
+					</div>
+				</div>
+
+				<div>
+					<button class="btn  btn-outline-danger" type="button">Remove</button>
+				</div>
+			</li>		
+		@endforeach
+{{-- 
 		<li class="d-flex justify-content-between align-items-center">
 			<div id="admin-wh" class="d-flex align-items-center">
 				<div class="admin-pic"></div>
@@ -126,7 +144,7 @@
 			<div>
 				<button class="btn  btn-outline-danger" type="button">Remove</button>
 			</div>
-		</li>
+		</li> --}}
 	</ul>
 
 
