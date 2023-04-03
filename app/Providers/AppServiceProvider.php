42c21dec74f5948670400f2ca3293eb96f9ba51f
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2023-04-02 18:16:54 -0700

    Configured default string length generated by migrations in AppServiceProvider to handle max key length error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        //Laravel uses the utf8mb4 charset which uses 4 bytes per character. 
+        //Max key length is 767 bytes so we need to manually configure the default string length to be at most 191 characters.
+        //191 x 4 bytes = 764 bytes.
+        Schema::defaultStringLength(191);
+
         //Prevent laravel debug bar from being loaded into heroku deployment.
         if($this->app->isLocal()) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
