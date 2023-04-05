8033a0ba3db3eb2e7c730506c7852ffcb69539ce
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2023-04-05 16:37:31 -0700

    Created a user factory and seeded the database with it

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,11 +5,17 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+use App\Models\User;
+use App\Models\AccessLevel;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+    //The name of the factory's corresponding model.
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
@@ -18,21 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'access_level_id' => AccessLevel::pluck('id')->random(), //Retrieves a random access level id to assign to user.
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
