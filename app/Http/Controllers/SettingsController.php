d34aba6006f82f5a5290ce7a157f9d0bc3852742
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2023-04-07 11:13:00 -0700

    Updated the query to retrieve all admin users without use of magic number in SettingsController

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -4,16 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\AccessLevel;
 
 class SettingsController extends Controller
 {
     public function index() {
-        //Retrieve all users that are admins
-        $admins = User::where('access_level_id', 2)->get();
-        $numAdmins = count($admins);
+        //Retrieve all users that have admin access.
+        // $admins = User::where('access_level_id', $adminId)->get();
+
+        $adminUsers = User::whereHas('accessLevel', function ($query) {
+            $query->where('access', 'Admin');
+        })->get();
+
+        //Retrieve the number of admin users.
+        $numAdmins = count($adminUsers);
 
         return view('settings.index', [
-            'admins' => $admins, 
+            'admins' => $adminUsers, 
             'numAdmins' => $numAdmins
         ]);
     }
