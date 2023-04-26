00f2c2e3f7b57268b58cbc5b3a0865851d128a3e
Victor Muljo <muljo@usc.edu>
2023-04-25 17:09:47 -0700

    minor bug fix

--- resources/views/broadcast/custom-message.blade.php
+++ resources/views/broadcast/custom-message.blade.php
@@ -57,10 +57,9 @@
     @push('scripts')
     <script>
         $(".clear").on('click',function(){
-        $(this).parents('form').find('input').val('');
-        $(this).parents('form').find('textarea').html('');
-        $(this).parents('form').find('select').val('');
-    });
+            $(this).parents('form').find('input').val('');
+            $(this).parents('form').find('textarea').html('');
+        });
     </script>
     @endpush
 
